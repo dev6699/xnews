61fdbd285842556535243b2ddea9322d95c3142d
kweijack <kweijack.dev@gmail.com>
2024-01-13 13:49:40 +0800

    fix: chg cloudflare middleware user agent

--- functions/proxy/_middleware.ts
+++ functions/proxy/_middleware.ts
@@ -4,7 +4,7 @@ export const onRequestGet: PagesFunction = async ({ request }) => {
 
     return fetch(url.search.split('?url=')[1], {
         headers: {
-            "user-agent": "okhttp/4.9.2",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
         }
     }).then(response => {
         const newResponse = new Response(response.body, {

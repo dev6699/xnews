e123c648ea7499e9b9a291ffe0a484b1301776be
kweijack <kweijack.dev@gmail.com>
2024-07-13 14:02:15 +0800

    feat: update proxy

--- functions/proxy/_middleware.ts
+++ functions/proxy/_middleware.ts
@@ -7,22 +7,25 @@ export const onRequest: PagesFunction = async ({ request }: { request: Request }
     headers.set("origin", targetURL.origin)
     headers.set("host", targetURL.host)
     headers.set("referer", targetURL.origin)
-    headers.set("x-requested-with", "XMLHttpRequest")
 
     return fetch(targetUrl, {
         headers,
-        body: request.method === "GET" ? undefined : request.body,
+        body: (request.method === "GET" || request.method === "OPTIONS") ? undefined : request.body,
         method: request.method
     }).then(async response => {
-        const newResponse = new Response(response.body, {
+        let newResponse = new Response(response.body, {
             status: response.status,
             statusText: response.statusText,
             headers: new Headers(response.headers)
         });
 
-        // Modify headers on the new response
+        if (request.method === "OPTIONS") {
+            newResponse = new Response()
+        }
+
         newResponse.headers.set('Access-Control-Allow-Origin', '*');
         newResponse.headers.set('Access-Control-Allow-Headers', '*');
+        newResponse.headers.set('Access-Control-Allow-Methods', '*');
         newResponse.headers.set('Access-Control-Max-Age', '86400');
         return newResponse
     });
